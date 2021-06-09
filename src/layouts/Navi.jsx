a6aa946dcf5d614c9c1ca5e267a62ac4398b09f2
TunaFigankaplan <swaxztn@gmail.com>
2021-06-09 22:38:44 +0300

    9 Haziran 22.38

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import CartSummary from "./CartSummary";
 import { Container, Menu } from "semantic-ui-react";
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+  function handleSignOut(params) {
+    setIsAuthenticated(false)
+  }
+
+  function handleSignIn(params) {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -14,8 +24,7 @@ export default function Navi() {
 
           <Menu.Menu position="right">
             <CartSummary/>
-            <SignedOut/>
-            <SignedIn/>
+            {isAuthenticated?<SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
           </Menu.Menu>
         </Container>
       </Menu>
--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Dropdown, Image, Menu } from 'semantic-ui-react'
 
-export default function SignedIn() {
+export default function SignedIn({signOut}) {
     return (
         <div>
             <Menu.Item>
@@ -9,7 +9,7 @@ export default function SignedIn() {
                 <Dropdown pointing="top right" text="Tuna">
                     <Dropdown.Menu>
                         <Dropdown.Item text="Bilgilerim" icon="info"/>
-                        <Dropdown.Item text="Çıkış Yap" icon="sign-out"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
                     </Dropdown.Menu>
                 </Dropdown>
             </Menu.Item>
