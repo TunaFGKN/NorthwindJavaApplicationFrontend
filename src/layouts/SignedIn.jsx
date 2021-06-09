71de700319cfbbd5d9f07d77d593e4757ee8221f
TunaFigankaplan <swaxztn@gmail.com>
2021-06-09 22:10:47 +0300

    9 Haziran 22.10

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+
+export default function SignedIn() {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://cdn.discordapp.com/attachments/844252129073561610/848197775812198460/c4fa7c4ae71de30b97ef45ca6c938a3c3a833f8d_full.jpg"/>
+                <Dropdown pointing="top right" text="Tuna">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
